a10c4c3a112c1b4a3abb00a24829abb62b7f5344
@Omner3 <gonzalezomner@hotmail.com>
2024-12-02 18:11:51 -0600

    se agregó función select

--- pc-tic-tac-toe-main/scripts/main.js
+++ pc-tic-tac-toe-main/scripts/main.js
@@ -1,5 +1,95 @@
+const board = document.querySelector('#board')
+const mensajeGanador = document.querySelector('#winner')
+const arrayBoard = [
+  ``,
+  ``,
+  ``,
+  ``,
+  ``,
+  ``,
+  ``,
+  ``,
+  ``]
+const ganadores = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
+board.innerHTML = ""
 
 
+function cambiarCharts() {
+
+  arrayBoard.forEach((element, i) => {
+    let template = `
+      <div class="col  g-3" id="${i}">
+        <div class="p-2  rounded-4 d-flex align-items-center justify-content-center board_item">
+        ${element}
+        </div>
+      </div>
+    `
+    board.innerHTML += template
+  })
+}
+cambiarCharts()
+
+function ganador() {
+  ganadores.forEach((secuencia) => {
+    let g1 = arrayBoard[secuencia[0]]
+    let g2 = arrayBoard[secuencia[1]]
+    let g3 = arrayBoard[secuencia[2]]
+
+    if (g1 !== "" && g1 === g2 && g2 === g3) {
+      mensajeGanador.classList.remove("d-none")
+    }
+  })
+}
+
+board.addEventListener('click', select)
+
+function select(e) {
+  const turn = e.target
+  const cross = `
+          <svg class="icon cross">
+            <use xlink:href="./icons/icon-x.svg#icon-x"></use>
+          </svg>
+          `
+          turn.innerHTML = cross
+} 
+
+
+
+
+/*let contador = 0
+let meromero = false
+let turno = 0
+while (contador < 9 && !meromero) {
+  if (turno === 0) {
+    let value = parseInt(prompt("Ingresa la posición en la que deseas jugar"))
+    if (arrayBoard[value] === "") {
+      arrayBoard[value] = `<svg class="icon cross">
+            <use xlink:href="./icons/icon-x.svg#icon-x"></use>
+          </svg>`
+    }
+  }else{
+    let position = true
+    while (positionbbbuhhhhh) {
+      let random = Math.floor(Math.random() = (maxFloored - minCeiled) + minCeiled)
+
+      
+    }
+  }
+  contador++
+}*/
+
+
+
+/* PRUEBA CON QUERYSELECTORALL
 const board = document.querySelectorAll('.board_item')
 
 board.forEach((chart) => {
@@ -11,7 +101,7 @@ board.forEach((chart) => {
           </svg>`
         board.innerHTML = chart_template
     })
-})
+})*/
 
 /*PRUEBA UN SOLO CHART (FUNCIONÓ)
 const board_item = document.querySelector('.board_item')
