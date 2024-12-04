20ee7f2f2ebc6ea37b0d6f9511ec2f389cd0d76b
@Omner3 <gonzalezomner@hotmail.com>
2024-12-03 19:43:02 -0600

    Funciones cpu y select agregadas

--- pc-tic-tac-toe-main/scripts/main.js
+++ pc-tic-tac-toe-main/scripts/main.js
@@ -1,5 +1,6 @@
 const board = document.querySelector('#board')
 const mensajeGanador = document.querySelector('#winner')
+const nextBtn = document.querySelector('#next')
 const arrayBoard = [
   ``,
   ``,
@@ -20,21 +21,24 @@ const ganadores = [
   [0, 4, 8],
   [2, 4, 6],
 ]
-board.innerHTML = ""
+
 
 
 function cambiarCharts() {
-
-  arrayBoard.forEach((element, i) => {
-    let template = `
+  board.innerHTML = ""
+  //if (board.innerHTML === "") {
+    arrayBoard.forEach((element, i) => {
+      let template = `
       <div class="col  g-3" id="${i}">
         <div class="p-2  rounded-4 d-flex align-items-center justify-content-center board_item">
         ${element}
         </div>
       </div>
     `
-    board.innerHTML += template
-  })
+      board.innerHTML += template
+    })
+    
+  //}
 }
 cambiarCharts()
 
@@ -47,46 +51,90 @@ function ganador() {
     if (g1 !== "" && g1 === g2 && g2 === g3) {
       mensajeGanador.classList.remove("d-none")
     }
-  })
+  });
 }
 
-board.addEventListener('click', select)
+board.addEventListener('click', (e)=>{
+  if (e.target.classList.contains('board_item')) {
+    select(e)
+  }
+}
+)
 
 function select(e) {
-  const turn = e.target
+  console.log('cliqueaste');
+  const turn = e.target;
   const cross = `
+    <svg class="icon cross">
+      <use xlink:href="./icons/icon-x.svg#icon-x"></use>
+    </svg>`;
+
+  turn.innerHTML = cross;
+
+  const index = e.target.id;
+  if (arrayBoard[index] !== "") {
+    console.log("La casilla ya está ocupada");
+    return;
+  }
+  arrayBoard[index] = cross;
+  cpu();
+}
+
+
+
+function cpu() {
+  let position = true;
+  while (position) {
+    let random = Math.floor(Math.random() * 9);
+    if (arrayBoard[random] === "") {
+      arrayBoard[random] =`<svg class="icon circle"> <use xlink:href="./icons/icon-o.svg#icon-o"></use> </svg>`; // Había utilizado innerHTML aquí pero no funcionó para poder ver los movimientos de cpu en el tablero
+      position = false;
+      console.log(arrayBoard)
+    }
+    console.log(random)
+  }
+  //cambiarCharts();
+  //ganador();
+}
+
+
+//nextBtn.addEventListener('click', mensajeGanador.classList.add('d-none'))
+
+
+/*function cpu() {
+  let contador = 0
+  let winner = false
+  let turno = 0
+  while (contador < 9 && !winner) {
+    if (turno === 0) {
+      let value = board.addEventListener('click', select)
+      if (arrayBoard[value] === "") {
+        arrayBoard[value] = `
           <svg class="icon cross">
             <use xlink:href="./icons/icon-x.svg#icon-x"></use>
           </svg>
           `
-          turn.innerHTML = cross
-} 
-
-
-
-
-/*let contador = 0
-let meromero = false
-let turno = 0
-while (contador < 9 && !meromero) {
-  if (turno === 0) {
-    let value = parseInt(prompt("Ingresa la posición en la que deseas jugar"))
-    if (arrayBoard[value] === "") {
-      arrayBoard[value] = `<svg class="icon cross">
-            <use xlink:href="./icons/icon-x.svg#icon-x"></use>
+        turno = 1
+      }
+    } else {
+      let position = true
+      while (position) {
+        let random = Math.floor(Math.random() = (9 - 0) + 0)
+        if (arrayBoard[random] === "") {
+          arrayBoard[random] = `<svg class="icon circle">
+            <use xlink:href="./icons/icon-o.svg#icon-o"></use>
           </svg>`
+          position = false
+          turno = 0
+        }
+      }
     }
-  }else{
-    let position = true
-    while (positionbbbuhhhhh) {
-      let random = Math.floor(Math.random() = (maxFloored - minCeiled) + minCeiled)
-
-      
-    }
+    winner = ganador()
+    cambiarCharts()
+    contador++
   }
-  contador++
-}*/
-
+}
+cpu() */
 
 
 /* PRUEBA CON QUERYSELECTORALL
